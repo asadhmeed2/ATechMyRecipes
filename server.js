5b0b093b7c8c7fe5b02e85337b0bd3eecc54fcc6
newasad <asadhmeed2@gmail.com>
2023-12-17 12:10:45 +0200

    serve the html file via server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const path = require('path')
+
+const {SERVER_PORT} = require('./config/config')
+
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname,'dist')));
+app.use(express.static(path.join(__dirname,'node_modules')));
+
+
+
+
+
+
+
+
+app.listen(SERVER_PORT, function (){
+    console.log(`server is running at port ${SERVER_PORT}`);
+})
